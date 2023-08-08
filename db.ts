9943c855d2bf1bf3db769e2c4f45ad828b7ad943
Vamsi <uvamsi76@gmail.com>
2023-08-08 19:09:06 +0530

    after a bit of frontend and backend and starting an new repo for backend and front end sepertely

---
+++ db.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+// Define mongoose schemas
+const userSchema = new mongoose.Schema({
+    username: {type: String},
+    password: String,
+    purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  });
+  
+  const adminSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    publishedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  });
+  
+  const courseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean,
+    Author: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin',required:true }
+  });
+  
+  // Define mongoose models
+  export const User = mongoose.model('User', userSchema);
+  export const Admin = mongoose.model('Admin', adminSchema);
+  export const Course = mongoose.model('Course', courseSchema);
+  
