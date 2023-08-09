490a06c11457948609b7ac3f29c25286684a8a75
Vamsi <uvamsi76@gmail.com>
2023-08-09 09:25:53 +0530

    test8

--- app.ts
+++ app.ts
@@ -187,6 +187,6 @@ app.get('/users/purchasedCourses', authenticateJwt, async (req, res) => {
 });
 
 app.get('/',(req,res)=>{
-  res.json("working fine mowa 8")
+  res.json("working fine mowa 9")
 })
 app.listen(3000, () => console.log('Server running on port 3000'));
